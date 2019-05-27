113220520ff2f908a3093bb43dce60978b300850
Chaewon Kong <chaewonkong@gmail.com>
2019-05-27 23:53:01 +0900

    fix: mv express static to outside of if statement

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
 }
 
+app.use(express.static(path.join(__dirname, "client/build")));
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
