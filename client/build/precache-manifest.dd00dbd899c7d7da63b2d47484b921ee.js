916b49cfeb5023e4791913135cc5b008b00e585c
Chaewon Kong <chaewonkong@gmail.com>
2019-05-27 23:43:14 +0900

    git: change gitignore

---
+++ client/build/precache-manifest.dd00dbd899c7d7da63b2d47484b921ee.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfaafc25608e80a95f8ae1c5a867ae76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06cfa9311cd434e4a19f",
+    "url": "/static/js/2.27460ab2.chunk.js"
+  },
+  {
+    "revision": "cc25bcda574e93b7dfe6",
+    "url": "/static/js/main.95160e74.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
